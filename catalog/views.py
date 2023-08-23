a74d096a79b8d65196f68382c1409fe8d007c96f
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-08-23 21:35:40 +0300

    changes for additional task

--- catalog/views.py
+++ catalog/views.py
@@ -5,4 +5,9 @@ def home(request):
     return render(request, 'catalog/home.html')
 
 def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'There is a message:{message} from {name}, with phone {phone}')
     return render(request, 'catalog/contacts.html')
