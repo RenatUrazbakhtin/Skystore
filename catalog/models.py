c7486e204ffc1c63c8aad0025e2987241c4df24e
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-01 17:30:02 +0300

    Fill models

--- catalog/models.py
+++ catalog/models.py
@@ -4,9 +4,28 @@ from django.db import models
 NULLABLE = {'null': True, 'blank': True}
 
 class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+    image = models.ImageField(upload_to='products/', verbose_name='изображение', **NULLABLE)
+    category = models.CharField(max_length=100, verbose_name='категория')
+    price = models.IntegerField(verbose_name='цена')
+    creation_date = models.DateTimeField(verbose_name='дата создания')
+    last_changes_date = models.DateTimeField(verbose_name='дата изменения')
+
     def __str__(self):
-        pass
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'Продукт'
+        verbose_name_plural = 'Продукты'
 
 class Category(models.Model):
+    name = models.CharField(max_length=100, verbose_name='наименование')
+    description = models.TextField(verbose_name='описание')
+
     def __str__(self):
-        pass
+        return f'{self.name} {self.description}'
+
+    class Meta:
+        verbose_name = 'Категория'
+        verbose_name_plural = 'Категории'
