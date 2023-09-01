34537cf8f2d223e38bdd958c8c43ba10836a477b
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-01 17:12:37 +0300

    add models

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+class Product(models.Model):
+    def __str__(self):
+        pass
+
+class Category(models.Model):
+    def __str__(self):
+        pass
