ce322350678016f61d26083d2f2bab3083fd3fbc
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-04 21:45:57 +0300

    add product func

--- catalog/views.py
+++ catalog/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
+
 # Create your views here.
 def home(request):
-    return render(request, 'catalog/home.html')
+    context = {
+        'object_list': Product.objects.all()
+    }
+    return render(request, 'catalog/home.html', context)
 
 def contacts(request):
     if request.method == 'POST':
@@ -11,3 +17,10 @@ def contacts(request):
         message = request.POST.get('message')
         print(f'There is a message:{message} from {name}, with phone {phone}')
     return render(request, 'catalog/contacts.html')
+
+def product(request, pk):
+    context = {
+        'object': Product.objects.get(pk=pk),
+        'title': f'Товар - {Product.name}',
+    }
+    return render(request, 'catalog/product.html', context)
