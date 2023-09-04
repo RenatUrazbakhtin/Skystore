2a7b9482ac8a56e20e520fd00dabb5168399495f
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-04 21:45:33 +0300

    add product func

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 
-from catalog.views import home, contacts
+from catalog.views import home, contacts, product
+
+
 
 urlpatterns = [
-    path('', home),
-    path('contacts/', contacts)
+    path('', home, name='home'),
+    path('contacts/', contacts, name='contacts'),
+    path('product/<int:pk>', product, name='product'),
 ]
