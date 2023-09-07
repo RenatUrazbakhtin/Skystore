03b5ae8df6e891a8db0e6e62840aecf095d670ff
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-07 21:48:00 +0300

    add correct url for update

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 
-from catalog.views import contacts, ProductListView, ProductDetailView, BlogCreateView, BlogDetailView, BlogListView
+from catalog.views import contacts, ProductListView, ProductDetailView, BlogCreateView, BlogDetailView, BlogListView, \
+    BlogUpdateView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
@@ -8,7 +9,7 @@ urlpatterns = [
     path('product/<int:pk>', ProductDetailView.as_view(), name='product'),
     path('create/', BlogCreateView.as_view(), name='create'),
     path('view/<int:pk>/', BlogDetailView.as_view(), name='view_blog'),
-    #path('update/',..., name='update'),
+    path('update/<int:pk>/', BlogUpdateView.as_view(), name='update'),
     #path('delete/',..., name='delete')
     path('list/', BlogListView.as_view(), name='list'),
 ]
