1388ead97231fe417a5a227a7f0281d2b24d0940
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-07 21:24:02 +0300

    settings url for create and read

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 
-from catalog.views import contacts, ProductListView, ProductDetailView
+from catalog.views import contacts, ProductListView, ProductDetailView, BlogCreateView, BlogDetailView, BlogListView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
     path('product/<int:pk>', ProductDetailView.as_view(), name='product'),
+    path('create/', BlogCreateView.as_view(), name='create'),
+    path('view/<int:pk>/', BlogDetailView.as_view(), name='view_blog'),
+    #path('update/',..., name='update'),
+    #path('delete/',..., name='delete')
+    path('list/', BlogListView.as_view(), name='list'),
 ]
