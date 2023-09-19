1c284deb48dadb43972516718468416d3226ff16
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-19 20:03:04 +0300

    add url for update product

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 
 from catalog.views import contacts, ProductListView, ProductDetailView, BlogCreateView, BlogDetailView, BlogListView, \
-    BlogUpdateView, BlogDeleteView, ProductCreateView
+    BlogUpdateView, BlogDeleteView, ProductCreateView, ProductUpdateView
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
     path('product/<int:pk>', ProductDetailView.as_view(), name='product'),
     path('create_product/', ProductCreateView.as_view(), name='create_product'),
+    path('update_product/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
     path('create/', BlogCreateView.as_view(), name='create'),
     path('view/<int:pk>/', BlogDetailView.as_view(), name='view_blog'),
     path('update/<int:pk>/', BlogUpdateView.as_view(), name='update'),
