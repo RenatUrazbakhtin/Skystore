97adc1b8329fb8df35df2f58841e24babdc9b44c
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-22 16:02:56 +0300

    small changes

--- catalog/models.py
+++ catalog/models.py
@@ -46,3 +46,17 @@ class Blog(models.Model):
     class Meta:
         verbose_name = 'Блог'
         verbose_name_plural = 'Блоги'
+
+
+class Version(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    version_number = models.IntegerField(verbose_name='Номер версии')
+    version_name = models.CharField(max_length=100, verbose_name='Название версии')
+    active_version = models.BooleanField(default=False, verbose_name='Текущая версия')
+
+    def __str__(self):
+        return f'название версии - {self.version_name}, номер версии -  {self.version_number}'
+
+    class Meta:
+        verbose_name = 'Версия'
+        verbose_name_plural = 'Версии'
