42f3f2b898d69702a65c9406ecb97aeddbf630be
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-24 16:26:38 +0300

    changes for correct work

--- catalog/forms.py
+++ catalog/forms.py
@@ -8,7 +8,8 @@ class StyleFormMixin:
     def __init__(self, **args):
         super().__init__(**args)
         for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'form-control'
+            if field_name != 'activae_version':
+                field.widget.attrs['class'] = 'form-control'
 
 class ProductForm(StyleFormMixin, forms.ModelForm):
     class Meta:
