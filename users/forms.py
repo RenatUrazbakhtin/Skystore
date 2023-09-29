d6e985061210e8361ac8b9a22a0eca4d9c0ffc37
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-29 22:26:55 +0300

    add form for userprofile

--- users/forms.py
+++ users/forms.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
 from catalog.forms import StyleFormMixin
 from users.models import User
@@ -9,3 +9,8 @@ class UserRegisterForm(StyleFormMixin, UserCreationForm):
     class Meta:
         model = User
         fields = ('email', 'password1', 'password2')
+
+class UserForm(StyleFormMixin, UserChangeForm):
+    class Meta:
+        model = User
+        fields = ('email', 'password', 'first_name', 'last_name', 'avatar', 'phone', 'country')
