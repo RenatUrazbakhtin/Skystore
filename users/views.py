4f02a07e8949f40eb9ab4419672ac788dfc40282
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-29 15:22:53 +0300

    send mail email confirm

--- users/views.py
+++ users/views.py
@@ -1,8 +1,12 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.shortcuts import render
-from django.urls import reverse_lazy
-from django.views.generic import CreateView
+import secrets
 
+from django.contrib.auth.forms import UserCreationForm
+from django.core.mail import send_mail
+from django.shortcuts import render, redirect
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
+
+from config import settings
 from users.forms import UserRegisterForm
 from users.models import User
 
@@ -13,3 +17,23 @@ class RegisterView(CreateView):
     form_class = UserRegisterForm
     template_name = 'users/register.html'
     success_url = reverse_lazy('users:login')
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user.is_active = False
+        token = secrets.token_urlsafe(nbytes=8)
+
+        user.token = token
+        activate_url = reverse_lazy('users:email_verified', kwargs={'token': user.token})
+        send_mail(
+            subject='Подтверждение почты',
+            message=f'Для подтверждения регистрации перейдите по ссылке: '
+                    f'http://localhost:8000/{activate_url}',
+            from_email=settings.EMAIL_HOST_USER,
+            recipient_list=[user.email],
+            fail_silently=False
+        )
+        user.save()
+
+        return redirect('users:email_sent')
+
