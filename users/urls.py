7adbf4bc72792e493b218923777799b24bbafe43
RenatUrazbakhtin <renat.urazbakhtin@mail.ru>
2023-09-29 21:38:18 +0300

    changes for sending message to email

--- users/urls.py
+++ users/urls.py
@@ -2,7 +2,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
 from users.apps import UsersConfig
-from users.views import RegisterView
+from users.views import RegisterView, UserConfirmEmailView, EmailConfirmationSentView
 
 app_name = UsersConfig.name
 
@@ -10,4 +10,6 @@ urlpatterns = [
     path('', LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
+    path('confirm-email/<str:token>/', UserConfirmEmailView.as_view(), name='email_verified'),
+    path('email-sent/', EmailConfirmationSentView.as_view(), name='email_sent'),
 ]
